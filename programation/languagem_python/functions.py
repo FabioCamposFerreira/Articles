4244d0a5aeaf238b323b42cf97bf0bd5581e8dd6
FabioCamposFerreira <63975718+FabioCamposFerreira@users.noreply.github.com>
2022-09-28 09:46:59 -0300

    Create functions.py

---
+++ programation/languagem_python/functions.py
@@ -0,0 +1,16 @@
+
+#-------------------------------------------
+function(1,2)
+
+def function(a,b):
+    print ("Sum = "+str(a+b))
+
+# NameError: name 'function' is not defined
+
+#-------------------------------------------
+def function(a,b):
+    print ("Sum = "+str(a+b))
+
+function(1,2)
+# Sum = 3
+#------------------------------
