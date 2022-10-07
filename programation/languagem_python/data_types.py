34c5a67e9f35bedb37cacce1498fac735aea8e94
FabioCamposFerreira <63975718+FabioCamposFerreira@users.noreply.github.com>
2022-10-07 12:36:45 -0300

    Create data_types.py

---
+++ programation/languagem_python/data_types.py
@@ -0,0 +1,14 @@
+#----------------------
+# List
+
+# Add list
+numbers = [1,2,3,4,5]
+numbers_str = ["1","2","3"]
+numbers.append(numbers_str)
+print(numbers)
+# ??
+
+numbers = [1,2,3,4,5]
+numbers.append(numbers_str)
+print(numbers)
+# ??
