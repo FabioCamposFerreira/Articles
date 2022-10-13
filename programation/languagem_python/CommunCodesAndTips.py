5e7f49928c18c378301f51425f19f7ede56f7986
FabioCamposFerreira <63975718+FabioCamposFerreira@users.noreply.github.com>
2022-10-13 09:25:16 -0300

    Update CommunCodesAndTips.py

--- programation/languagem_python/CommunCodesAndTips.py
+++ programation/languagem_python/CommunCodesAndTips.py
@@ -2,3 +2,19 @@
 import math
 print(math.ceil(2/10))
 # 1
+#-------------------------------
+# Numpy
+# Get histogram of the sequency
+import numpy as np
+numbers = [2,1,2,3,2,10,1]
+hist = np.histogram(numbers,bins=range(10))
+print(hist[0])
+# ??
+print(hist[1])
+# ??
+numbers_2 = [10,5,1.2,1.3,1.9,2,2.9]
+hist = np.histogram(numbers_2,bins=[0,2])
+print(hist[0])
+# ??
+print(hist[1])
+# ??
